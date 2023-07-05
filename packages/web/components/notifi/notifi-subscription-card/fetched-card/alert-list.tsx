b79613c3517d6deaa9ebeef6654d8b4613f388f1
juni-notifi <hyungjoon.kim@notifi.network>
2023-07-05 23:42:01 +0900

    Arrange toggles per kind
    Instead of having separate toggles per destination, we want separate
    toggles per alert kind

--- packages/web/components/notifi/notifi-subscription-card/fetched-card/alert-list.tsx
+++ packages/web/components/notifi/notifi-subscription-card/fetched-card/alert-list.tsx
@@ -1,136 +1,34 @@
-import { EventTypeItem } from "@notifi-network/notifi-frontend-client";
-import {
-  useNotifiClientContext,
-  useNotifiSubscriptionContext,
-} from "@notifi-network/notifi-react-card";
-import { FunctionComponent, useCallback, useEffect, useState } from "react";
+import { FunctionComponent } from "react";
 
 import { useNotifiConfig } from "../../notifi-config-context";
-import { AlertRow, ToggleState } from "./alert-row";
+import { AlertRow } from "./alert-row";
 
 export const AlertList: FunctionComponent = () => {
   const config = useNotifiConfig();
-  const { loading, email, phoneNumber, telegramId, alerts, render } =
-    useNotifiSubscriptionContext();
-  const {
-    canary: { frontendClient },
-  } = useNotifiClientContext();
-
-  const [toggles, setToggles] = useState<Record<string, boolean>>({});
-  const isToggled = useCallback(
-    (targetType: "email" | "sms" | "telegram", row: EventTypeItem): boolean => {
-      return toggles[`${row.name}__${targetType}`] ?? false;
-    },
-    [toggles]
-  );
-
-  useEffect(() => {
-    if (config.state === "fetched") {
-      const newToggles = config.data.eventTypes.reduce<Record<string, boolean>>(
-        (record, row) => {
-          const alert = alerts[row.name];
-          const hasEmail = (alert?.targetGroup?.emailTargets?.length ?? 0) > 0;
-          const hasSms = (alert?.targetGroup?.smsTargets?.length ?? 0) > 0;
-          const hasTelegram =
-            (alert?.targetGroup?.telegramTargets?.length ?? 0) > 0;
-
-          record[`${row.name}__email`] = hasEmail;
-          record[`${row.name}__sms`] = hasSms;
-          record[`${row.name}__telegram`] = hasTelegram;
-          return record;
-        },
-        {}
-      );
-      setToggles(newToggles);
-    }
-  }, [alerts, config]);
-
-  const toggleTarget = useCallback(
-    async (
-      targetType: "email" | "sms" | "telegram",
-      row: EventTypeItem,
-      toggled: boolean
-    ) => {
-      const key = `${row.name}__${targetType}`;
-      if (toggles[key] === toggled) {
-        return;
-      }
-
-      setToggles((current) => ({
-        ...current,
-        [key]: toggled,
-      }));
-
-      try {
-        const includeEmail =
-          targetType === "email" ? toggled : isToggled("email", row);
-        const includeSms =
-          targetType === "sms" ? toggled : isToggled("sms", row);
-        const includeTelegram =
-          targetType === "telegram" ? toggled : isToggled("telegram", row);
-        await frontendClient.ensureTargetGroup({
-          name: row.name,
-          emailAddress: includeEmail && email !== "" ? email : undefined,
-          telegramId:
-            includeTelegram && telegramId !== "" ? telegramId : undefined,
-          phoneNumber:
-            includeSms && phoneNumber !== "" ? phoneNumber : undefined,
-        });
-
-        const alert = alerts[row.name];
-        if (alert === undefined) {
-          await frontendClient.ensureAlert({ eventType: row, inputs: {} });
-        }
-      } catch {
-        frontendClient
-          .fetchData()
-          .then(render)
-          .catch(() => {});
-      }
-    },
-    [
-      alerts,
-      email,
-      frontendClient,
-      isToggled,
-      phoneNumber,
-      render,
-      telegramId,
-      toggles,
-    ]
-  );
-
-  const getToggleState = useCallback(
-    (
-      targetType: "email" | "sms" | "telegram",
-      row: EventTypeItem
-    ): ToggleState => {
-      return {
-        value: isToggled(targetType, row),
-        onToggle: (toggled) => {
-          toggleTarget(targetType, row, toggled);
-        },
-      };
-    },
-    [isToggled, toggleTarget]
-  );
 
   if (config.state !== "fetched") {
     return null;
   }
 
   return (
-    <ul>
-      {config.data.eventTypes.map((row) => (
-        <AlertRow
-          key={row.name}
-          row={row}
-          disabled={loading}
-          email={getToggleState("email", row)}
-          telegram={getToggleState("telegram", row)}
-          sms={getToggleState("sms", row)}
-        />
-      ))}
+    <ul className="flex flex-col gap-3 px-6 pb-6">
+      {config.data.eventTypes.map((row) => {
+        if (row.type === "label") {
+          return (
+            <li key={row.name} className="flex flex-col gap-3">
+              <p className="text-subtitle1 font-subtitle1">{row.name}</p>
+              <p className="text-caption font-caption text-osmoverse-200">
+                {row.tooltipContent}
+              </p>
+            </li>
+          );
+        }
+        return (
+          <li key={row.name}>
+            <AlertRow row={row} disabled={false} />
+          </li>
+        );
+      })}
     </ul>
   );
 };
--- packages/web/components/notifi/notifi-subscription-card/fetched-card/alert-row.tsx
+++ packages/web/components/notifi/notifi-subscription-card/fetched-card/alert-row.tsx
@@ -6,98 +6,38 @@ import { Switch } from "~/components/control";
 interface Props {
   row: EventTypeItem;
   disabled: boolean;
-  email: ToggleState;
-  sms: ToggleState;
-  telegram: ToggleState;
-}
-
-export interface ToggleState {
-  value: boolean;
-  onToggle: (toggled: boolean) => void;
 }
 
 interface GenericProps extends Props {
   title: string;
-  description: string;
 }
 
 export const AlertRow: FunctionComponent<Props> = (props) => {
   switch (props.row.type) {
     case "broadcast":
-      return (
-        <GenericAlertRow
-          {...props}
-          title={props.row.name}
-          description={props.row.tooltipContent ?? ""}
-        />
-      );
+      return <GenericAlertRow {...props} title={props.row.name} />;
     case "directPush":
-      return (
-        <GenericAlertRow
-          {...props}
-          title={props.row.name}
-          description={props.row.tooltipContent ?? ""}
-        />
-      );
+      return <GenericAlertRow {...props} title={props.row.name} />;
     case "fusion":
     case "fusionToggle":
-      return (
-        <GenericAlertRow
-          {...props}
-          title={props.row.name}
-          description={props.row.tooltipContent ?? ""}
-        />
-      );
+      return <GenericAlertRow {...props} title={props.row.name} />;
     default:
-      return (
-        <GenericAlertRow
-          {...props}
-          disabled
-          title={props.row.name}
-          description="Unsupported notification type. Please contact the developers."
-        />
-      );
+      return <GenericAlertRow {...props} disabled title={props.row.name} />;
   }
 };
 
 const GenericAlertRow: FunctionComponent<GenericProps> = ({
   title,
-  description,
   disabled,
-  email,
-  sms,
-  telegram,
 }) => {
   return (
-    <div className="flex flex-col gap-3 p-4 hover:bg-osmoverse-700">
-      <p className="text-subtitle1 font-subtitle1">{title}</p>
-      <p className="text-caption font-caption text-osmoverse-200">
-        {description}
-      </p>
-      <Switch
-        labelPosition="right"
-        disabled={disabled}
-        isOn={email.value}
-        onToggle={email.onToggle}
-      >
-        Email
-      </Switch>
-      <Switch
-        labelPosition="right"
-        disabled={disabled}
-        isOn={telegram.value}
-        onToggle={telegram.onToggle}
-      >
-        Telegram
-      </Switch>
-      <Switch
-        labelPosition="right"
-        disabled={disabled}
-        isOn={sms.value}
-        onToggle={sms.onToggle}
-      >
-        SMS
-      </Switch>
-    </div>
+    <Switch
+      labelPosition="right"
+      disabled={disabled}
+      isOn={false}
+      onToggle={() => {}}
+    >
+      {title}
+    </Switch>
   );
 };
